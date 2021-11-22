cb64547c9f4fb3f699f19b97aa1a07fa44db7804
CharlotteMAUVEZIN <charlotte.mauvezin@protonmail.com>
2021-11-22 18:05:28 +0100

    Treating the double page, preparing the testing function

--- src/sql_to_csv/sql_to_csv.py
+++ src/sql_to_csv/sql_to_csv.py
@@ -3,12 +3,15 @@
 
 # Importation of the library
 import argparse
+import ast
 import csv
 import logging
 import os.path
 import sqlite3
 from pathlib import Path
 
+import pandas as pd
+from shapely.geometry import Polygon
 from tqdm import tqdm
 
 
@@ -22,6 +25,7 @@ class SqlToCsv:
         self.list_book_id = []
         self.list_page_id = []
         self.transcription = []
+        self.type_page = ""
         logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.DEBUG)
 
     def __enter__(self):
@@ -53,13 +57,20 @@ class SqlToCsv:
         return self.list_page_id
 
     def get_transcription_from_pageid_with_paragraph(self, page_id):
+        """Get and return the transcription for simple page"""
         self.cursor.execute(
             f"select text from transcription where element_id in (select id from element where id in ("
             f"select child_id from element_path where parent_id='{page_id}')and type='paragraph' order by polygon)"
         )
-        transcription = self.cursor.fetchall()
+        df = pd.DataFrame(self.cursor.fetchall(), columns=["text"])
+        return self.get_transcription_df_single_page(df)
+
+    def get_transcription_df_single_page(self, df):
+        transcription = ""
+        for index, row in df.iterrows():
+            transcription += row["text"] + " "
         transcription = [phrase[0].replace("\n", " ") for phrase in transcription]
-        return " ".join(transcription)
+        return "".join(transcription)
 
     def save_book_as_csv(self, book_id):
         """Save the book (page id and transcription) in a csv"""
@@ -70,10 +81,17 @@ class SqlToCsv:
             writer = csv.writer(save_book_csv)
             self.list_page_id = self.get_list_page(book_id)
             logging.info("looking for transcription")
-            for page_id in tqdm(self.list_page_id):
-                page_id = page_id[0]
-                trans = self.get_transcription_from_pageid_with_paragraph(page_id)
-                writer.writerow([page_id, trans])
+            type_page = self.check_type_page_from_book_id(book_id)
+            if type_page == "single_page":
+                for page_id in tqdm(self.list_page_id):
+                    page_id = page_id[0]
+                    trans = self.get_transcription_from_pageid_with_paragraph(page_id)
+                    writer.writerow([page_id, trans])
+            elif type_page == "double_page":
+                for page_id in tqdm(self.list_page_id):
+                    page_id = page_id[0]
+                    trans = self.get_transcription_double_page(page_id)
+                    writer.writerow([page_id, trans])
 
     def save_all_books(self):
         """Save all the book (page id and transcription) in their respective csv named 'id_book'.csv"""
@@ -101,9 +119,60 @@ class SqlToCsv:
         """Save a book in a txt file"""
         with open(os.path.join(self.output_path, f"{book_id}.txt"), "w") as file:
             self.list_page_id = self.get_list_page(book_id)
-            for page_id in self.list_page_id:
-                trans = self.get_transcription_from_pageid_with_paragraph(page_id[0])
-                file.write(trans)
+            type_page = self.check_type_page_from_book_id(book_id)
+            if type_page == "simple_page":
+                for page_id in self.list_page_id:
+                    trans = self.get_transcription_from_pageid_with_paragraph(
+                        page_id[0]
+                    )
+                    file.write(trans)
+            elif type_page == "double_page":
+                for page_id in self.list_page_id:
+                    trans = self.get_transcription_double_page(page_id[0])
+                    file.write(trans)
+                # logging.info(str(type_page))
+
+    def check_type_page_from_book_id(self, book_id):
+        """Check the type of the page to apply the right get_transcription algorithm"""
+        self.cursor.execute(
+            f"select class_name from classification where element_id = '{book_id}'"
+        )
+        return self.cursor.fetchall()[0][0]
+
+    def get_transcription_double_page(self, page_id):
+        """Get the transcription on a double page with the paragraph in order"""
+        self.cursor.execute(
+            f"select text, sel.polygon from transcription inner join (select id, polygon from element where id in (select child_id from element_path where parent_id='{page_id}')and type='paragraph' order by polygon) as sel on transcription.element_id=sel.id"
+        )
+        df = pd.DataFrame(self.cursor.fetchall(), columns=["text", "polygon"])
+        # df.to_csv(os.path.join(self.output_path, f"aaa{page_id}.csv"))
+
+        return self.get_transcription_double_page_df(df)
+
+    def get_transcription_double_page_df(self, df):
+        """Extract the transcription from data"""
+        df["polygon"] = df.polygon.apply(lambda x: Polygon(ast.literal_eval(str(x))))
+        df["x_axis"] = df.polygon.apply(lambda a: a.centroid.x)  # order on which page
+        df["y_axis"] = df.polygon.apply(
+            lambda a: a.centroid.y
+        )  # order where on the page
+        transcription = ""
+        # Check if the dataframe is empty else return empty string
+        if len(df.index) != 0:
+            # Create a limit between the pages
+            df = df.sort_values(by=["y_axis"])
+            stat = df["x_axis"].describe()
+            x_limit = (stat[4] + stat[7]) / 2
+
+            # Get the transcription
+            for i in [0, 1]:
+                for index, row in df.iterrows():
+                    if i == 0 and row["x_axis"] < x_limit:
+                        transcription += row["text"] + " "
+                    if i == 1 and row["x_axis"] > x_limit:
+                        transcription += row["text"] + " "
+            transcription = [phrase[0].replace("\n", " ") for phrase in transcription]
+        return "".join(transcription)
 
 
 def main():
