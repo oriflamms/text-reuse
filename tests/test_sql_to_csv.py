0bd20e68cec600fb5038f962dd8b7bc9e47938ec
CharlotteMAUVEZIN <charlotte.mauvezin@protonmail.com>
2021-11-23 15:02:51 +0100

    true assert

--- tests/test_sql_to_csv.py
+++ tests/test_sql_to_csv.py
@@ -23,6 +23,10 @@ def test_dummy():
     test_simple = SqlToCsv.get_transcription_df_single_page(df_simple)
     test_double = SqlToCsv.get_transcription_double_page_df(df_double)
 
+    # the data to assert
+    single_assert = "statis gaudio fod vivere fidelium Deus omnium conditor et redemper torro animabus famulo rum famularum quae tu aurum remissionem annorum eorum tribue peccatorum ut indulgentiam quam semper optaverunt prius supplicationibus conse quaetur Qui vivis et re regnas Deus per omnia secula seculorum amen requiescant in pacem "
+    double_asert = "me pries luy que il vueille me pries luy que il vueille egesimo octavoir mon cuer calum egesimo octavoir mon cuer calum servir et amer servir et amer Ave maria Ave maria Eratres doulce dame pour Eratres doulce dame pour Arcelle grant ioie que Arcelle grant ioie que vous eustes au iour de nost vous eustes au iour de nost quant prae doule filia nascum quant prae doule filia nascum de populus Doulce dame pries de populus Doulce dame pries luy que il mortiorem habens luy que il mortiorem habens Mitte natuite ama redemptio Mitte natuite ama redemptio Ave maria Ave maria Patres doulce dame post Patres doulce dame post Sur icelle grant ioitum Sur icelle grant ioitum que vous eustes quant vos que vous eustes quant vos trigis robis viderent ostris trigis robis viderent ostris a vostre doule fili cor muliere a vostre doule fili cor muliere et encens et il les receut et encens et il les receut Doulce dame pries luper quae Doulce dame pries luper quae il vueille recesservoir mam il vueille recesservoir mam quant elephara Domini quant elephara Domini corpus corpus Lae maria Lae maria Et tres doulce dame per Et tres doulce dame per Assur icelle grant more Assur icelle grant more que vous eustes quant vos que vous eustes quant vos Christus in templum Christus in templum invenirent curribus invenirent curribus "
+
     # asserts
-    assert type("a") == type(test_simple)
-    assert type("a") == type(test_double)
+    assert single_assert == test_simple
+    assert double_asert == test_double
