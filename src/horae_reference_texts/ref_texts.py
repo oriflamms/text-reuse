91d55ac6e26421f88fc6b096ea60fec10d270a5d
Charlotte Mauvezin <charlotte.mauvezin@irht.cnrs.fr>
2022-01-18 17:23:06 +0100

    Extraxt nb of word per text for sql_to_csv half annotated

--- src/horae_reference_texts/ref_texts.py
+++ src/horae_reference_texts/ref_texts.py
@@ -77,7 +77,15 @@ class ReferenceTexts:
         df_meta[["ID Arkindex", "ID Annotation", "Work H-ID"]].to_csv(
             os.path.join(metadata_path, "metadata_heurist.csv"), index=False
         )
-        # print(df_meta[["ID Arkindex", "ID Annotation", "Work H-ID"]].size)
+
+    def write_nb_of_word_metadata(self, metadata_path):
+        name_nb_list = [
+            [r["ID Annotation"].split()[-1], len(str(r["Text"]).split())]
+            for i, r in self.df.iterrows()
+        ]
+        with open(os.path.join(metadata_path, "metadata_nb_name.csv"), "w") as nb_file:
+            writer = csv.writer(nb_file)
+            writer.writerows(name_nb_list)
 
     def get_statistics(self):
         """Return the stats and create files on frequencies"""
@@ -178,6 +186,14 @@ def main():
         default="",
         type=Path,
     )
+    parser.add_argument(
+        "-n",
+        "--nb-word",
+        help="path of the output file nb_word file where there is the number of word and the h_tag",
+        required=False,
+        default="",
+        type=Path,
+    )
 
     args = vars(parser.parse_args())
 
@@ -189,6 +205,9 @@ def main():
 
     if args["metadata_path"] and args["liturgical_function"]:
         f.write_metadata(args["metadata_path"])
+
+    if args["nb_word"] and args["liturgical_function"]:
+        f.write_nb_of_word_metadata(args["nb_word"])
 
 
 if __name__ == "__main__":
